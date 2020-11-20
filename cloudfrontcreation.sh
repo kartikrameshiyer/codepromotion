a5915c68a0199022edffcddced11a17f016cfd58
Kartik Ramesh <kartik.ramesh@icloud.com>
2020-11-20 15:03:13 -0500

    nothing

--- cloudfrontcreation.sh
+++ cloudfrontcreation.sh
@@ -1,5 +1,5 @@
 aws cloudformation deploy \
 --template-file cloudfront.yml \
 --stack-name production-distro \
---parameter-overrides PipelineID="testfile-1" \ # Name of the S3 bucket you created manually.
+--parameter-overrides PipelineID="testfile-1" \  # Name of the S3 bucket you created manually.
 --tags project=udapeople 
